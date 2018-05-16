562433e52720e9491a2e9df592959010edda7c8f
onerain <onerain88@gmail.com>
2018-05-16 16:19:22 +0800

    增加开启/关闭房间，设置房间可见的单元测试

---
+++ LeanCloud.Play/LeanCloud.Play/Test/LeanCloud.Play.TestUnit.Mono/UnitTest.Mono/OpenRoomTest.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using System;
+
+using LeanCloud;
+
+namespace UnitTest.Mono {
+	[TestFixture()]
+	public class OpenRoomTest: TestBase {
+		public OpenRoomTest () : base()
+		{
+			
+		}
+
+		[Test()]
+		[Timeout(300000)]
+		public void JoinOrCreate ()
+		{
+			Play.UserID = "xxx";
+			Play.Connect("0.0.1");
+
+			Assert.That(Done, Is.True.After(2000000));
+		}
+
+		[PlayEvent]
+		public override void OnAuthenticated ()
+		{
+			Play.Log("OnAuthenticated");
+			var conf = new PlayRoom.RoomConfig() {
+				IsOpen = false
+			};
+			Play.CreateRoom(conf, "123");
+		}
+
+		[PlayEvent]
+		public override void OnCreatedRoom ()
+		{
+			Play.Log("OnCreatedRoom");
+			var roomName = Play.Room.Name;
+
+		}
+
+		[PlayEvent]
+		public override void OnCreateRoomFailed (int errorCode, string reason)
+		{
+			Play.Log("OnCreateRoomFailed: " + reason);
+		}
+
+		[PlayEvent]
+		public override void OnJoinedRoom ()
+		{
+			Play.Log("OnJoinedRoom");
+			bool open = (bool)Play.Player.CustomProperties ["IsOpen"];
+			Play.Log("IsOpen: " + open);
+		}
+
+		[PlayEvent]
+		public override void OnJoinRoomFailed (int errorCode, string reason)
+		{
+			Play.Log("OnJoinRoomFailed: " + reason);
+		}
+	}
+}
