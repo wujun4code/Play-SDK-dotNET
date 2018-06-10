e0b728ed07b0e2d15fd8a93da5fda337ae5f0217
WuJun <wujun19890209@gmail.com>
2018-06-10 19:35:44 +0800

    chore: append some unit test.

---
+++ LeanCloud.Play/LeanCloud.Play/Test/LeanCloud.Play.TestUnit.Mono/UnitTest.Mono/CustomRouteServerTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using LeanCloud;
+using System.Collections;
+using System.Linq;
+
+namespace UnitTest.Mono
+{
+    [TestFixture()]
+    public class CustomRouteServerTest : TestBase
+    {
+        [Test]
+        [Timeout(300000)]
+        public void CustomRouteServer()
+        {
+            Play.UserID = RandomClientId;
+            Play.SetRouteServer("http://localhost:5000/play/");
+            Play.Connect("0.0.1");
+
+            Assert.That(true, Is.True.After(2000000));
+        }
+
+        [PlayEvent]
+        public override void OnAuthenticated()
+        {
+
+        }
+
+    }
+}
